65da904aece73dfbb8c1599e24645d172d0a6c86
SrpCD <132849061+srpcd@users.noreply.github.com>
2024-12-13 22:48:45 +0200

    core files

---
+++ index.js
@@ -0,0 +1,39 @@
+function updateNavbar() {
+    const mediaQuery = window.matchMedia("(max-width: 820px)");
+    if (mediaQuery.matches) {
+        const navButtons = document.getElementsByClassName("navbuttons");
+        const hamburgerBtn = document.getElementById("hamburgerBtn");
+        for (let i = 0; i < navButtons.length; i++) {
+            navButtons[i].style.display = 'none';
+        }
+        hamburgerBtn.style.display = 'flex';
+    } else {
+        const navButtons = document.getElementsByClassName("navbuttons");
+        const hamburgerBtn = document.getElementById("hamburgerBtn");
+        for (let i = 0; i < navButtons.length; i++) {
+            navButtons[i].style.display = 'flex';
+        }
+        hamburgerBtn.style.display = 'none';
+    }
+}
+
+function openNavbar() {
+    const hamburgerBtnMenu = document.getElementById("hamburgerBtnMenu");
+    hamburgerBtnMenu.setAttribute('tabindex', '-1');
+    let focusEvent = () => {
+        setTimeout(() => {
+            hamburgerBtnMenu.style.display = 'none';
+            hamburgerBtnMenu.removeEventListener('focusout', focusEvent);
+            window.removeEventListener('resize', focusEvent);
+        }, 100);
+    }
+    hamburgerBtnMenu.style.display = 'block';
+    hamburgerBtnMenu.focus();
+    hamburgerBtnMenu.addEventListener('focusout', focusEvent);
+    window.addEventListener('resize', focusEvent);
+}
+
+window.addEventListener('resize', () => {updateNavbar()});
+updateNavbar();
+
+
